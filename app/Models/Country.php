9e93a41faa22b2b117e402214c35071f760549ff
arinata098 <arinata098@gmail.com>
2024-02-20 12:01:31 +0800

    Relation manager

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Country extends Model
 {
@@ -12,4 +14,14 @@ class Country extends Model
     protected $fillable = [
         'name',
     ];
+
+    public function states(): HasMany
+    {
+        return $this->hasMany(State::class);
+    }
+    
+    public function employees(): HasMany
+    {
+        return $this->hasMany(Employee::class);
+    }
 }
